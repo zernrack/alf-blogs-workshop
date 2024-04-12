b1974dbf35f3921f63a4b709f6a2cee3102c49be
Renz Rackhold <67743847+zernrack@users.noreply.github.com>
2024-04-12 22:06:40 +0800

    setup project and add db

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,13 @@
 // db here
+const mongoose = require("mongoose");
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to DB!");
+  } catch (error) {
+    console.log("Error DB:", error);
+  }
+};
+
+module.exports = connectDb;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db.js");
+
+const app = express();
+const port = process.env.PORT || 5000;
+connectDB();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello world" });
+});
+
+app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
